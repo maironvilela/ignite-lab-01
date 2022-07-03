bfc30a6cfeeae01f139641932fd0a070138bd1d2
Mairon Vilela <maironvilela@hotmail.com>
2022-07-03 18:55:38 -0300

    test: call listsCourses function of the ListCoursesRepository protocol

---
+++ classroom/src/data/service/course/list-courses/list-courses.service.spec.ts
@@ -0,0 +1,36 @@
+import { ListCoursesRepository } from '~/data/protocols';
+import { Course } from '~/domain/models';
+import { ListCoursesService } from './list-courses.service';
+
+interface MakeSutTypes {
+  sut: ListCoursesService;
+  listCoursesRepositoryStub: ListCoursesRepository;
+}
+
+const makeListCoursesRepositoryStub = (): ListCoursesRepository => {
+  class ListCoursesRepositoryStub implements ListCoursesRepository {
+    async listCourses(): Promise<Course[]> {
+      return await new Promise((resolve) => resolve(null));
+    }
+  }
+
+  return new ListCoursesRepositoryStub();
+};
+
+const makeSut = (): MakeSutTypes => {
+  const listCoursesRepositoryStub = makeListCoursesRepositoryStub();
+  const sut = new ListCoursesService(listCoursesRepositoryStub);
+  return { sut, listCoursesRepositoryStub };
+};
+
+describe('ListCoursesService', () => {
+  it('should call the function listCourses', async () => {
+    const { sut, listCoursesRepositoryStub } = makeSut();
+
+    const hashSpy = jest.spyOn(listCoursesRepositoryStub, 'listCourses');
+
+    await sut.execute();
+
+    expect(hashSpy).toHaveBeenCalled();
+  });
+});
